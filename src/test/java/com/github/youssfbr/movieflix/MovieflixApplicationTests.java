5109f1e044635affab5ef568c38c810df34ac631
Alisson Youssf <youssfbr@gmail.com>
2024-03-15 16:00:07 -0300

    Project created

---
+++ src/test/java/com/github/youssfbr/movieflix/MovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.movieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
