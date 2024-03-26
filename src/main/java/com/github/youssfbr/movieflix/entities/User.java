43a5632584294daed163288b8a34daf6d0a5bf4b
Alisson Youssf <youssfbr@gmail.com>
2024-03-26 13:49:46 -0300

    UserDetails

--- src/main/java/com/github/youssfbr/movieflix/entities/User.java
+++ src/main/java/com/github/youssfbr/movieflix/entities/User.java
@@ -1,15 +1,17 @@
 package com.github.youssfbr.movieflix.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails , Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -61,9 +63,31 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+				.map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
 	public String getPassword() {
 		return password;
 	}
+
+	@Override
+	public String getUsername() { return email; }
+
+	@Override
+	public boolean isAccountNonExpired() { return true; }
+
+	@Override
+	public boolean isAccountNonLocked() { return true; }
+
+	@Override
+	public boolean isCredentialsNonExpired() { return true; }
+
+	@Override
+	public boolean isEnabled(){ return true; }
 
 	public void setPassword(String password) {
 		this.password = password;
